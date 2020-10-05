8d91b4c8237bf0f4122948f45c74354aa1d3ee52
Lee Archer <l.archer@scalr.com>
2020-10-05 12:02:05 +0300

    Add module

--- main.tf
+++ main.tf
@@ -7,11 +7,16 @@ resource "null_resource" "resource1" {
   }
 }
 
-resource "null_resource" "resource1" {
+resource "null_resource" "resource2" {
   provisioner "local-exec" {
     command = "echo $ENV"
     environment = {
       ENV = "Hello world 2!"
     }
   }
+}
+
+module "vpc" {
+	source  = "9c5e338bfd16.test-env.scalr.com/env-svrcnchebt61e30/vpc/aws"
+	version = "2.9.0"
 }
