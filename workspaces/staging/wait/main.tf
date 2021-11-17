c9c03af2c2114361468f2a8857d0aa5211731667
Lee Archer <l.archer@scalr.com>
2021-11-17 13:44:17 +0200

    Update

--- workspaces/staging/wait/main.tf
+++ workspaces/staging/wait/main.tf
@@ -1,8 +1,31 @@
-variable "duration" {
-  type    = number
-  default = 240
+terraform {
+  backend "remote" {
+    hostname = "2188636d0130.test-env.scalr.com"
+    organization = "env-svrcnchebt61e30"
+
+    workspaces {
+      name = "test"
+    }
+  }
 }
 
+variable "duration" {
+  type    = number
+  default = 2400
+}
+
+resource "random_id" "server" {
+  byte_length = 10
+
+  provisioner "local-exec" {
+    command = "cat .terraform.lock.hcl"
+  }
+}
 resource "time_sleep" "wait" {
   create_duration = "${var.duration}s"
 }
+
+output "test" {
+  value = random_id.server.dec
+}
+
